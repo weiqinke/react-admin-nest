171dbf60e5fe12830216a3093c9cc51638db1990
weiqinke <928148936@qq.com>
2023-10-29 17:21:54 +0800

    feat: 新增路由跳转动画

--- src/components/Echarts/index.ts
+++ src/components/Echarts/index.ts
@@ -1,15 +1,25 @@
 import { lazy } from "react";
 
-export const AreaStackChart = lazy(() => import(/* webpackChunkName: "AreaStackChart'"*/ "./AreaStackChart"));
+// export const AreaStackChart = lazy(() => import(/* webpackChunkName: "AreaStackChart'"*/ "./AreaStackChart"));
 
-export const BarChart = lazy(() => import(/* webpackChunkName: "BarChart'"*/ "./BarChart"));
+// export const BarChart = lazy(() => import(/* webpackChunkName: "BarChart'"*/ "./BarChart"));
 
-export const LineChart = lazy(() => import(/* webpackChunkName: "LineChart'"*/ "./LineChart"));
+// export const LineChart = lazy(() => import(/* webpackChunkName: "LineChart'"*/ "./LineChart"));
 
-export const PictorialBarChart = lazy(() => import(/* webpackChunkName: "PictorialBarChart'"*/ "./PictorialBarChart"));
+// export const PictorialBarChart = lazy(() => import(/* webpackChunkName: "PictorialBarChart'"*/ "./PictorialBarChart"));
 
-export const PieChart = lazy(() => import(/* webpackChunkName: "PieChart'"*/ "./PieChart"));
+// export const PieChart = lazy(() => import(/* webpackChunkName: "PieChart'"*/ "./PieChart"));
 
-export const ScatterChart = lazy(() => import(/* webpackChunkName: "ScatterChart'"*/ "./ScatterChart"));
+// export const ScatterChart = lazy(() => import(/* webpackChunkName: "ScatterChart'"*/ "./ScatterChart"));
 
-export const LoginFrequencyChart = lazy(() => import(/* webpackChunkName: "ScatterChart'"*/ "./LoginFrequencyChart"));
+// export const LoginFrequencyChart = lazy(() => import(/* webpackChunkName: "ScatterChart'"*/ "./LoginFrequencyChart"));
+
+import AreaStackChart from "./AreaStackChart";
+import BarChart from "./BarChart";
+import LineChart from "./LineChart";
+import PictorialBarChart from "./PictorialBarChart";
+import PieChart from "./PieChart";
+import ScatterChart from "./ScatterChart";
+import LoginFrequencyChart from "./LoginFrequencyChart";
+
+export { AreaStackChart,BarChart ,LineChart,PictorialBarChart,PieChart,ScatterChart,LoginFrequencyChart};
