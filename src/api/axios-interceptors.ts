a007d50c4406cf91ba5bf7d657eae7d96800aba9
Wei qin ke <928148936@qq.com>
2024-01-21 19:01:34 +0800

    refactor: 重新改变拦截器返回的数据结构

--- src/api/axios-interceptors.ts
+++ src/api/axios-interceptors.ts
@@ -61,35 +61,17 @@ const resp415 = {
   }
 };
 
-const resp20401 = {
-  onFulfilled(response) {
-    if (response.status === 200 && response.data.code === 20401) {
-      // 为了处理返回数据为空，hash
-      // $message.error({
-      //   content: '获取到0条数据',
-      //   duration: 1
-      // });
-    }
-    if (response.status === 200 && response.data.code === 41701) {
-      message.error({
-        content: "查询参数错误",
-        duration: 1
-      });
-    }
-    if (response.data.code !== 200 && response.data.message) {
-      message.error({
-        content: `${response.data.message}`,
-        duration: 1
-      });
-    }
-    return response;
+const respTransform = {
+  onFulfilled({ data = {} }) {
+    // 改变数据结构，
+    return data;
   }
 };
 
 const resperror = {
   onRejected(error) {
     message.error({
-      content: "请求错误",
+      content: "网络错误",
       duration: 1
     });
     return Promise.reject(error);
@@ -131,4 +113,4 @@ const reqCommon = {
 
 export const requestInterce = [reqCommon]; // 请求拦截
 
-export const responseInterce = [resp401, resp403, resp404, resp415, resp20401, resperror]; // 响应拦截
+export const responseInterce = [resp401, resp403, resp404, resp415, respTransform, resperror]; // 响应拦截
