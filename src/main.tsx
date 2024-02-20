2a2d8c3bfd9f325462ddbeb089f768b5b5baa521
Wei qin ke <928148936@qq.com>
2024-02-20 14:32:58 +0800

    feat: 更新模式

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import ReactDOM from "react-dom/client";
 import ProjectRouter from "./router/index.tsx";
 import { RouterProvider } from "react-router-dom";
+import React from "react";
 
 import "./reset.css";
 const router = ProjectRouter();
 
-ReactDOM.createRoot(document.getElementById("caravan-root")!).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById("caravan-root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
